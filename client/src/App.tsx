e2b82d19ef202c2f15c17f454be133902ab478f1
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-06-09 00:04:14 +0300

    feat: render root layout in app component and its corresponding children components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
 import TypingTest from "./components/typing-test";
+import RootLayout from "./layouts/root-layout";
+
+// I can create gamification like HayDay when the user opens the site every day for X day and when he make a record and so on
 
 export default function App() {
-  return <TypingTest />;
+  return (
+    <RootLayout>
+      <TypingTest />
+    </RootLayout>
+  );
 }
