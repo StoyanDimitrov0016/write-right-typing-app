73ffbce50cfa574648fceb1d2bcf4a6f8a1b1daf
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-06-08 21:13:21 +0300

    feat: render TypingTest component in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import TypingTest from "./components/typing-test";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold bg-yellow-300">Hello world!</h1>;
+  return <TypingTest />;
 }
